edc9802f8e6c0bc1ac5bf3fa7cc9e7bd0c4df8d1
Ademola Bhadmus <sanguine@Ademolas-MBP.lan>
2024-04-19 19:05:00 +0100

    hardcoded value

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 
-async function setupNodeEvents (on, config){
+async function setupNodeEvents(on, config) {
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
 
   on("file:preprocessor", browserify.default(config));
@@ -11,12 +11,15 @@ async function setupNodeEvents (on, config){
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl:'https://staging.trymima.com/',
+    baseUrl: 'https://staging.trymima.com/',
     defaultCommandTimeout: 10000,
     viewportHeight: 900,
     viewportWidth: 1400,
-    specPattern:  '**/*.feature',
+    specPattern: '**/*.feature',
     watchForFileChanges: false,
+    env: {
+      "MAILSLURP_API_KEY": "dca3b5c13a989a8eae5fa47f403f6e35f43adef136c75d9fda889d601a7b1bfe"
+    },
     setupNodeEvents,
   },
 });
