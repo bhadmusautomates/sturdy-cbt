80c705f4567bcdba88d97904698d9a6724279d1f
Ademola Bhadmus <sanguine@Ademolas-MBP.lan>
2024-04-20 17:35:08 +0100

    removed env config

--- cypress.config.js
+++ cypress.config.js
@@ -17,9 +17,6 @@ module.exports = defineConfig({
     viewportWidth: 1400,
     specPattern: '**/*.feature',
     watchForFileChanges: false,
-    env: {
-      "MAILSLURP_API_KEY": "ee56382ae589ba46dd3a64e12061c912adfab92c25c4dc5c9c98e537ca5114bb"
-    },
     setupNodeEvents,
   },
 });
